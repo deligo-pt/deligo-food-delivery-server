2e3238cddbb82f97fcdca67a5028b40ca2cb7970
umayermdemon <mamudmdemon@gmail.com>
2026-01-05 14:48:04 +0600

    fix build error

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -52,7 +52,7 @@ const auth = (...requiredRoles: TUserRole[]) => {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
     }
 
-    req.user = user as JwtPayload;
+    req.user = decoded as JwtPayload;
     next();
   });
 };
