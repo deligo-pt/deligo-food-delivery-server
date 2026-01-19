801b82f17b5ecc76e065e1624c2beafcfa3d5854
umayermdemon <mamudmdemon@gmail.com>
2026-01-19 09:33:06 +0600

    update tax validation

--- src/app/modules/Tax/tax.interface.ts
+++ src/app/modules/Tax/tax.interface.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export type TTax = {
   _id: mongoose.Types.ObjectId;
   taxName: string; // Sage Name: "IVA Normal", "IVA Intermédio"
-  taxCode: string; // Sage Code: "NOR", "INT", "RED"
+  taxCode: 'NOR' | 'INT' | 'RED' | 'ISE'; // Sage Code: "NOR", "INT", "RED"
   taxRate: 6 | 13 | 23 | 0; // 23, 13, 6
   countryID: string; // "PRT"
   TaxRegionID?: string;
