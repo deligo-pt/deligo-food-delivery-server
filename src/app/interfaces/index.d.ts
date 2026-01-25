97ff98887481a1ccae9e7c10c080ac2bc87d93ad
umayermdemon <mamudmdemon@gmail.com>
2026-01-25 07:18:11 +0600

    solved sos problem

--- src/app/interfaces/index.d.ts
+++ src/app/interfaces/index.d.ts
@@ -1,14 +1,10 @@
 import { JwtPayload } from 'jsonwebtoken';
-import { Server } from 'socket.io';
-
 declare global {
   namespace Express {
     interface Request {
       user: JwtPayload;
     }
   }
-  // eslint-disable-next-line no-var
-  var io: Server;
 }
 
 export {};
