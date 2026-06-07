c4a45d28a49db4f1bb1dba5277886b7268ed5f29
umayermdemon <mamudmdmeon@gmail.com>
2026-06-07 09:00:26 +0600

    feat: simplify TDeliveryPartner schema by removing unused fields and enhancing model clarity

--- src/app/modules/Delivery-Partner/delivery-partner.interface.ts
+++ src/app/modules/Delivery-Partner/delivery-partner.interface.ts
@@ -18,27 +18,9 @@ export type TDeliveryPartner = {
   };
   role: 'DELIVERY_PARTNER';
   email: string;
-  password: string;
   status: keyof typeof USER_STATUS;
-  isEmailVerified: boolean;
   isDeleted: boolean;
   isUpdateLocked: boolean;
-
-  // --------------------------------------------------------
-  // Pending temporary Email and contact number
-  // --------------------------------------------------------
-  pendingEmail?: string;
-  pendingContactNumber?: string;
-
-  // ------------------------------------------------------
-  // OTP & Password Reset
-  // ------------------------------------------------------
-
-  requiresOtpVerification?: boolean;
-
-  passwordResetToken?: string;
-  passwordResetTokenExpiresAt?: Date;
-  passwordChangedAt?: Date;
 
   // -------------------------------------------------
   // 1) Personal Information
@@ -167,12 +149,6 @@ export type TDeliveryPartner = {
     activity?: string;
     insurancePolicy?: string;
   };
-
-  // -------------------------------------------------
-  // 10) Security & Access
-  // -------------------------------------------------
-  twoFactorEnabled?: boolean;
-  loginDevices?: TLoginDevice[];
 
   // -------------------------------------------------
   // 11) Admin Workflow (Approval System)
