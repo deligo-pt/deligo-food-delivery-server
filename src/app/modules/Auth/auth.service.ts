2ac0f50583810c47922182093be2a48e4f265921
umayermdemon <mamudmdmeon@gmail.com>
2026-06-10 11:51:45 +0600

    feat: add user existence check in changePassword service to enhance error handling

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -819,6 +819,15 @@ const changePassword = async (
 
   const userStatus = currentUser?.status;
 
+  const userExists = await AuthUser.findOne({
+    userId: currentUser.userId,
+    role: currentUser.role,
+  }).select('+password');
+
+  if (!userExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'User not found');
+  }
+
   if (
     userStatus === USER_STATUS.REJECTED ||
     userStatus === USER_STATUS.BLOCKED
@@ -828,7 +837,7 @@ const changePassword = async (
 
   const isPasswordMatched = await AuthUser.isPasswordMatched(
     payload.oldPassword,
-    currentUser.password as string,
+    userExists.password as string,
   );
 
   //checking if the password is correct
