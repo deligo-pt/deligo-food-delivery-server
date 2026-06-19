821ccd81120e55cd88893633e628865875d6bc91
MD EMON MIAH <142793133+umayeremon@users.noreply.github.com>
2026-06-19 11:44:39 +0600

    Update permission.validation.ts

--- src/app/modules/Permission/permission.validation.ts
+++ src/app/modules/Permission/permission.validation.ts
@@ -95,4 +95,5 @@ export const PermissionValidations = {
   createPermissionValidationSchema,
   updatePermissionValidationSchema,
   assignPermissionsValidationSchema,
+  revokePermissionsValidationSchema
 };
