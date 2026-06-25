d22c343d2a43f96ee40aeb1ce5dc85ea9adea383
umayermdemon <mamudmdmeon@gmail.com>
2026-06-25 09:40:34 +0600

    feat: Add formatAddonGroupResponse utility for multilingual support in addon groups

---
+++ src/app/modules/Add-Ons/addOns.utils.ts
@@ -0,0 +1,28 @@
+import { TLanguageCode } from '../../constant/GlobalInterface/language.interface';
+
+export const formatAddonGroupResponse = (data: any, lang: TLanguageCode) => {
+  const formatSingleGroup = (item: any) => {
+    const itemObj = item.toObject?.() || item;
+
+    const title = itemObj.title?.[lang] || itemObj.title?.['en'] || '';
+
+    const options = Array.isArray(itemObj.options)
+      ? itemObj.options.map((opt: any) => ({
+          ...opt,
+          name: opt.name?.[lang] || opt.name?.['en'] || '',
+        }))
+      : [];
+
+    return {
+      ...itemObj,
+      title,
+      options,
+    };
+  };
+
+  if (Array.isArray(data)) {
+    return data.map((item) => formatSingleGroup(item));
+  }
+
+  return formatSingleGroup(data);
+};
